fd61f87a38324bef17be4d10c57d6cdaee6f3f6d
sfK <76771179+sfK-jm@users.noreply.github.com>
2022-12-28 21:47:40 +0900

    [Silver IV] Title: 주몽, Time: 44 ms, Memory: 31772 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+M = int(input())
+A = list(map(int, input().split()))
+A.sort()
+count = 0
+i = 0
+j = N - 1
+
+while i < j:  # 두 포인터 이동 원칙 따라 포인터를 이동하며 처리
+    if A[i] + A[j] < M:
+        i += 1
+    elif A[i] + A[j] > M:
+        j -= 1
+    else:
+        count += 1
+        i += 1
+        j -= 1
+
+print(count)
