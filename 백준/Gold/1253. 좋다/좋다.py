acc20bf5f3e4e58b8f3fb445fdd6b124ff7398fb
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-01-17 21:02:10 +0900

    [Gold IV] Title: 좋다, Time: 1100 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+Result = 0
+A = list(map(int, input().split()))
+A.sort()
+
+for k in range(N):
+    find = A[k]
+    i = 0
+    j = N - 1
+    while i < j:  # 투 포인터 알고리즘
+        if A[i] + A[j] == find:  # find는 서로 다른 두 수의 합이어야 합을 체크
+            if i != k and j != k:
+                Result += 1
+                break
+            elif i == k:
+                i += 1
+            elif j == k:
+                j -= 1
+        elif A[i] + A[j] < find:
+            i += 1
+        else:
+            j -= 1
+print(Result)
