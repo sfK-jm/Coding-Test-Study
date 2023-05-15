bbb33175c4b965f5b3dad5df1141636f5a6c7cc9
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-05-15 13:19:02 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+
+int main(){
+    while (scanf("%d", &n) != EOF){
+        ll cnt = 1 ,ret = 1;
+        while (true){
+            if (cnt % n == 0){
+                printf("%lld\n", ret);
+                break;
+            } else {
+                cnt = cnt * 10 + 1;
+                cnt = cnt % n;
+                ret ++;
+            }
+        }
+    }
+}
