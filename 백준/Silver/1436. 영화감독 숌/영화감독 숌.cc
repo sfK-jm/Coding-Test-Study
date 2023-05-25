ffdf3ad1f6efa0a2b2bcf5e8d99ed57ae0898e8b
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-05-25 13:46:26 +0900

    [Silver V] Title: 영화감독 숌, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+int n;
+
+int main(){
+    cin >> n;
+    int i = 666;
+    for (;; i++){
+        if (to_string(i).find("666") != string::npos) n--;
+        if (n == 0) break;
+    }
+    cout << i << endl;
+}
