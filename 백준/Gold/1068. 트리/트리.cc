f555a1dc44ba3a534f2c72924c585022f4a7c2a9
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-05-29 20:36:53 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n" 
+int n, r, temp, root;
+vector<int> adj[54];
+int dfs(int here){
+    int ret = 0;
+    int child = 0;
+    for (int there : adj[here]){
+        if (there == r) continue;
+        ret += dfs(there);
+        child++;
+    }
+    if (child == 0) return 1;
+    return ret;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> temp;
+        if (temp == -1) root = i;
+        else adj[temp].push_back(i);
+    }
+    cin >> r;
+    if (r == root){
+        cout << "0" << endl; return 0;
+    } 
+
+    cout << dfs(root) << endl;
+    return 0;
+
+}
