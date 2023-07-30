9520e5f7a8a78752966b3b105cf0569373283cff
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-07-30 22:02:55 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.15 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    if(arr.size() == 1) {
+        answer.push_back(-1);
+        return answer;
+    }
+    
+    arr.erase(min_element(arr.begin(), arr.end()));
+    answer = arr;
+    return answer;
+}
