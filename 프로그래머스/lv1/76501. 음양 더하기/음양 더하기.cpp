4876a49e4335e5212e20ce4cf093bdd32c28788e
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-08-04 22:11:39 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for (int i = 0; i < absolutes.size(); i++){
+        if (signs[i] == false) absolutes[i] *= -1; 
+    }
+    
+    for (int i = 0; i < absolutes.size(); i++) answer += absolutes[i];
+    
+    return answer;
+}
