11fd7bac860bd1e2c73b6125dae6b144089dc613
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-08-10 22:16:38 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main() {
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++){
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
