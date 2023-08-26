a30f75a9d75fa1a71700df3fed73e29d86678ec4
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-08-26 20:06:17 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 276 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -7,6 +7,10 @@ vector<int> v;
 int n, temp;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> temp;
