ecf42cbac115d4f8c029624ca3b5d570c8273db4
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-08-29 14:30:24 +0900

    [Silver V] Title: 칠무해, Time: 6448 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14729. 칠무해/칠무해.cc
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int n;
+double temp;
+priority_queue<double>pq;
+vector<double> v;
+
+int main() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+		if (pq.size() == 7) {
+			pq.push(temp);
+			pq.pop();
+		}
+		else pq.push(temp);
+	}
+
+	while (pq.size()) {
+		v.push_back(pq.top());
+		pq.pop();
+	}
+	reverse(v.begin(), v.end());
+	for (double i : v) printf("%.3lf\n", i);
+}
