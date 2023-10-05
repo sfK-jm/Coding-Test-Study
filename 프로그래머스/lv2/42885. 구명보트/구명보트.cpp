7d7fcd27a3a48d3e836e475de5e2317df628dd79
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-10-05 19:53:05 +0900

    [level 2] Title: 구명보트, Time: 1.26 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0, idx = 0;
+    sort(people.begin(), people.end());
+    
+    while (people.size() > idx){
+        int back = people.back();
+        people.pop_back();
+        
+        if (people[idx] + back <= limit){
+            answer ++;
+            idx ++;
+        } else {
+            answer++;
+        }
+    }
+        
+    return answer;
+}
