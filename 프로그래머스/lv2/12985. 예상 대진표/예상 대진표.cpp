f52fee2728f058b57c6beb5e59bd28036969122b
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-10-07 22:15:05 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
@@ -7,8 +7,8 @@ int solution(int n, int a, int b)
     int answer = 0;
 
     while (a != b){
-        a = a % 2 == 0 ? a / 2 : (a + 1) / 2;
-        b = b % 2 == 0 ? b / 2 : (b + 1) / 2;
+        a = (a + 1) / 2;
+        b = (b + 1) / 2;
         answer++;
     }
 
