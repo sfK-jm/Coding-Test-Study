60c45126fc0d836e3441df2c8a41bfcf849b3725
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-10-08 23:16:49 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    double answer = 0;
+    
+    answer = (double)num1 / num2 * 1000;
+    
+    return (int)answer;
+}
