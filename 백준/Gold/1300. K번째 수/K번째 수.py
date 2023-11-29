260687b32765c9b8b16a2fb0fa0f0785549aa045
sfK <76771179+sfK-jm@users.noreply.github.com>
2023-11-29 21:30:38 +0900

    [Gold I] Title: K번째 수, Time: 1056 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+k = int(input())
+
+start, end = 0, k
+
+while start <= end:
+    mid = (start + end) // 2
+    cnt = 0
+    
+    for i in range(1, n+1):
+        cnt += min(mid // i, n)
+        
+    if cnt >= k:
+        answer = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+        
+print(answer)
