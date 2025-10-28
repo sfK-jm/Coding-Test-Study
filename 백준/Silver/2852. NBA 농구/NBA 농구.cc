ede1754504a6646e7b618e31a3935ab23dd2a2bd
sfK <76771179+sfK-jm@users.noreply.github.com>
2025-10-28 11:01:58 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int n, o, A, B, asum, bsum;
@@ -10,8 +10,8 @@ string print(int a){
     return d.substr(d.size() -2 , 2) + ":" + e.substr(e.size() -2, 2);
 }
 
-int changeToInt(string a){
-    return atoi(a.substr(0, 2).c_str()) * 60 + atoi(a.substr(3,2).c_str());
+int changeToInt(string s){
+    return stoi(s.substr(0, 2)) * 60 + stoi(s.substr(3,2));
 }
 
 void go(int &sum, string s){
@@ -24,12 +24,12 @@ int main(){
     cin >> n; // 골 케이스 수
     for (int i = 0; i < n; i++){
         cin >> o >> s; // o: 득점한 팀, s: 골 넎은 시간
-        if (A > B)go(asum, s); 
+        if (A > B)go(asum, s);
         else if (B > A) go(bsum, s);
         o == 1 ? A++ : B++; // 팀의 점수 추가
         previous = s;
     }
-    
+
     if (A > B) go(asum, "48:00");
     else if (B > A) go(bsum, "48:00");
     cout << print(asum) << "\n";
